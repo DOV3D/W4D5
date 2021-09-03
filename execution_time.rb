dc3c2c5cfd83fbe0d0057e7daeb29a4d2288f632
mrossant <matteorossant@gmail.com>
2021-09-03 14:30:16 -0400

    Finished working on big O

--- execution_time.rb
+++ execution_time.rb
@@ -25,9 +25,75 @@ end
 #  p my_min_2(list)
 
 def largest_contiguous_subsum(list)
-
-
+    subs = []
+    (0...list.length).each do |start_idx|
+        (start_idx...list.length).each do |end_idx|
+            subs << list[start_idx..end_idx]
+        end
+    end
+    
+    largest_sum_pair = subs[0].sum
+    subs.each {|sub| largest_sum_pair = sub.sum if sub.sum > largest_sum_pair}
+    largest_sum_pair
 end
+
+# def lcs(list)
+# =begin
+# write function using O(n) with O(1)
+# var 1 => keep tally of largest sum
+# var 2 => tracks current sum
+# =end
+
+#     if list.all? {|el| el < 0}
+#         return list.max
+#     end
+#     largest_sum = list[0]
+#     current_sum = 0    
+#     # (0...list.length -  1).each do |i|
+#     #     if list[i] + list[i + 1] > current_sum
+#     #         current_sum = list[i] + list[i + 1]
+#     #         if current_sum > largest_sum
+#     #             largest_sum = current_sum
+#     #         end
+#     #     else
+#             current_sum = 0
+#             next
+#         end
+#     end
+
+#     largest_sum
+# end
+
+def lcs(list)
+=begin
+write function using O(n) with O(1)
+var 1 => keep tally of largest sum
+var 2 => tracks current sum
+=end
+
+    if list.all? {|el| el < 0}
+        return list.max
+    end
+    largest_sum = list[0]
+    current_sum = 0    
+    (0...list.length).each do |i|
+        if list[i] > current_sum
+            current_sum += list[i]
+            if current_sum > largest_sum
+                largest_sum = current_sum
+            end
+        elsif current_sum + list[i] > 0
+            current_sum += list[i]
+        else
+            current_sum = 0
+        end
+    end
+
+    largest_sum
+end
+
+
+
 
 
 
