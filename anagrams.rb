51b7474806bbc515cbd8a02fbd56acd7a09f6e44
mrossant <matteorossant@gmail.com>
2021-09-03 16:38:11 -0400

    Finished sorting bigO

--- anagrams.rb
+++ anagrams.rb
@@ -1,8 +1,8 @@
-def first_anagram?(str1, str2)
+def first_anagram?(str1, str2) # O(n!) because O(n!) dominates O(n)
 
-   list = str1.chars.permutation.to_a
+   list = str1.chars.permutation.to_a # O(n!)
 
-    list.include?(str2.chars)
+    list.include?(str2.chars) # O(n)
 end
 
 
@@ -11,16 +11,43 @@ end
 # p first_anagram?("elvis", "lives")    #=> true
 
 
-def second_anagram?(str1, str2)
-    chars = str2.chars 
-    str1.each_char do |char1|
-        index_str2 = chars.find_index(char1)
-       return false if index_str2.nil?
-        chars.delete_at(index_str2) 
+def second_anagram?(str1, str2) # 
+    chars = str2.chars # O(n)
+    str1.each_char do |char1| #O(n)
+        index_str2 = chars.find_index(char1) # ask TA / what's the time complexity of find_index?
+        return false if index_str2.nil?
+        chars.delete_at(index_str2)  # ask TA / what's the time complexity of delete_at?
     end
-    chars.empty?
+    chars.empty? #O(n)
 end
 
 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+def third_anagram?(str1, str2)
+    jumble_sort(str1) == jumble_sort(str2)
+end
+
+def jumble_sort(str)
+    alpha = ("a".."z").to_a
+    sorted = false
+
+    until sorted
+        sorted = true
+        (0...str.length - 1).each do |i|
+            if alpha.index(str[i]) > alpha.index(str[i + 1])
+                str[i], str[i + 1] = str[i + 1], str[i]
+                sorted = false
+            end
+        end
+    end
+    str
+end
+
+def fourth_anagram?(str1, str2)
+    hash = Hash.new(0)
+    str1.each_char {|char| hash[char] += 1}
+    str2.each_char {|char| hash[char] -= 1}
+    hash.all? {|k, v| v == 0}
+end
